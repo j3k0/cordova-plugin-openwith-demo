d0ebd852db8cefcaf1629d75edc586ac9426e2b2
Jean-Christophe Hoelt <hoelt@fovea.cc>
2017-10-07 11:47:17 +0300

    Update with ios support

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ document.addEventListener('deviceready', setupOpenwith, false);
 function setupOpenwith() {
 
   // Increase verbosity if you need more logs
-  //cordova.openwith.setVerbosity(cordova.openwith.DEBUG);
+  cordova.openwith.setVerbosity(cordova.openwith.DEBUG);
 
   // Initialize the plugin
   cordova.openwith.init(initSuccess, initError);
@@ -17,15 +17,20 @@ function setupOpenwith() {
   function myHandler(intent) {
     console.log('intent received');
 
-    console.log('  action: ' + intent.action);   // type of action requested by the user
+    console.log('  action: ' + intent.action); // type of action requested by the user
     console.log('  exit: ' + intent.exit); // if true, you should exit the
-                                                 // app after processing
+                                           // app after processing
 
     for (var i = 0; i < intent.items.length; ++i) {
       var item = intent.items[i];
-      console.log('  type: ' + item.type);   // mime type
-      console.log('  uri: ' + item.uri);     // uri to the file, probably NOT a web uri
-      console.log('  path: ' + item.path);   // path on the device, might be undefined
+      console.log('  type: ', item.type);   // mime type
+      console.log('  uri:  ', item.uri);     // uri to the file, probably NOT a web uri
+
+      // some optional additional info
+      console.log('  text: ', item.text);   // text to share alongside the item, iOS only
+      console.log('  name: ', item.name);   // suggested name of the image, iOS 11+ only
+      console.log('  utis: ', item.utis);
+      console.log('  path: ', item.path);   // path on the device, generally undefined
     }
 
     // ...
@@ -38,8 +43,14 @@ function setupOpenwith() {
       cordova.openwith.load(intent.items[0], function(data, item) {
         // data is a long base64 string with the content of the file
         console.log("item loaded, it weights " + data.length + " bytes");
-        // uploadToServer(item); // upload to your server, confirm to the user.
+        // upload to your server, confirm to the user.
+        // uploadToServer(item, function() {
+        //   if (intent.exit) { cordova.openwith.exit(); }
+        // });
       });
+    }
+    else {
+      // if (intent.exit) { cordova.openwith.exit(); }
     }
   }
 }

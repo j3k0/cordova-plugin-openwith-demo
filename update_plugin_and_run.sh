679cb94353567b8685981eb0232d1e80f1668389
Jean-Christophe Hoelt <hoelt@fovea.cc>
2017-10-07 14:43:30 +0300

    Remove unused script

--- update_plugin_and_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-(cd ..; npm t) \
-  && cordova plugin rm cc.fovea.cordova.openwith \
-  && cordova plugin add .. --variable MIME_TYPE="image/*" \
-  && cordova run android --debug --device
